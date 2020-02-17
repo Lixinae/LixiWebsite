29b576329de1fe54fcb7c3f1511ed14c800704de
Vahen <naeos@laposte.net>
2020-02-17 23:35:45 +0100

    - Correction chargement static des images

--- application/__init__.py
+++ application/__init__.py
@@ -5,9 +5,9 @@ from flask_sqlalchemy import SQLAlchemy
 from application.configuration import Configuration
 
 app = Flask(__name__,
-            static_url_path='',
             static_folder='static',
             template_folder='templates')
+
 app.config.from_object(Configuration)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
