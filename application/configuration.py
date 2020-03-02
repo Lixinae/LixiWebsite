6120de2db65164505f7d2a68b206a0281bcda02e
Vahen <naeos@laposte.net>
2020-03-02 21:24:30 +0100

    [Index] - Nettoyage du code -> suppression de div inutile - Passage de la configuration en mode test

--- application/configuration.py
+++ application/configuration.py
@@ -7,3 +7,4 @@ class Configuration(object):
     SECRET_KEY = os.environ.get('SECRET_KEY', 'you-will-never-guess')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    TESTING = True  # Todo -> Supprimer une fois en prod
