3c8d927497e4dc295de72b484ee49e98a7a50783
Vahen <naeos@laposte.net>
2020-04-05 14:47:03 +0200

    [Application] - Suppressission de pages inutiles - Ajout d'un dossier "unused" pour les templates créé avant mais plus utilisé

--- application/about/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-about_bp = Blueprint('about_bp', __name__,
-                     static_folder='static',
-                     template_folder='templates')
-
-from application.about import routes
