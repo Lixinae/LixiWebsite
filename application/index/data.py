404c95906ef562626a55e7f1f2b560b7eb9679ed
Vahen <naeos@laposte.net>
2020-04-16 18:03:58 +0200

    [Index] - Ajout de la liste des skills sur la page d'index - Modification du template

---
+++ application/index/data.py
@@ -0,0 +1,35 @@
+from typing import List, Dict
+
+from flask import url_for
+
+
+# Permet de query la liste de tous les projets
+def skills() -> List[Dict]:
+    skills_list = [
+        {
+            'name': "Python",
+            'img_url': url_for('static', filename='img/skills/python.png'),
+            'img_alt': "Python 3 icon",
+        },
+        {
+            'name': "Flask",
+            'img_url': url_for('static', filename='img/skills/flask.png'),
+            'img_alt': "Python 3 icon",
+        },
+        {
+            'name': "Docker",
+            'img_url': url_for('static', filename='img/skills/docker.png'),
+            'img_alt': "Docker icon",
+        },
+        {
+            'name': "C++",
+            'img_url': url_for('static', filename='img/skills/cpp.png'),
+            'img_alt': "C++ icon",
+        },
+        {
+            'name': "Java",
+            'img_url': url_for('static', filename='img/skills/java_2.png'),
+            'img_alt': "Java icon",
+        },
+    ]
+    return skills_list
--- application/index/routes.py
+++ application/index/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, make_response
 
-from application.index import index_bp
+from application.index import index_bp, data
 
 from application.passions import data as data_passions
 from application.portfolio import data as data_portfolio
@@ -15,4 +15,6 @@ from application.portfolio import data as data_portfolio
 def index():
     passions_list = data_passions.passions_short()
     project_list = data_portfolio.project_short()
-    return make_response(render_template('index.html', title="Home", passionsList=passions_list, projectList=project_list), 200)
+    skills_list = data.skills()
+    return make_response(
+        render_template('index.html', title="Home", passionsList=passions_list, projectList=project_list, skills=skills_list), 200)
