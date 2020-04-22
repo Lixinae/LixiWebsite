a1a7091e1f5b83c4190d2574566c9050afbd049f
Vahen <naeos@laposte.net>
2020-04-22 17:24:26 +0200

    [Porfolio]     [Pacman 3D]     - Ajout de la gallerie de screenshot     - Suppression de la div sur les skills_front

--- application/portfolio/data_folder/data_pacman3d.py
+++ application/portfolio/data_folder/data_pacman3d.py
@@ -3,10 +3,6 @@ from typing import List, Dict
 from flask import url_for
 
 
-# from application import models
-
-
-# Permet de query la liste de tous les projets
 def skills_front() -> List[Dict]:
     return []
 
@@ -41,3 +37,29 @@ def skills_back() -> List[Dict]:
 
     ]
     return skills
+
+
+def screenshots() -> List[Dict]:
+    screenshots_list = [
+        {
+            'name': "Camera de base",
+            'img_url': url_for('static', filename='img/projects/screenshots/pacman3d/camera_base.png'),
+            'img_alt': "Camera de base screenshot",
+        },
+        {
+            'name': "Camera fps",
+            'img_url': url_for('static', filename='img/projects/screenshots/pacman3d/camera_fps.png'),
+            'img_alt': "Camera fps screenshot",
+        },
+        {
+            'name': "Camera Oblique 1",
+            'img_url': url_for('static', filename='img/projects/screenshots/pacman3d/camera_oblique_1.png'),
+            'img_alt': "Camera Oblique 1 screenshot",
+        },
+        {
+            'name': "Camera Oblique 2",
+            'img_url': url_for('static', filename='img/projects/screenshots/pacman3d/camera_oblique_2.png'),
+            'img_alt': "Camera Oblique 2 screenshot",
+        },
+    ]
+    return screenshots_list
