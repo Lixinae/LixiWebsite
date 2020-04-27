cc1b1250a75a4e052977445ea2c7cb29c8bae301
Vahen <naeos@laposte.net>
2020-04-27 17:46:39 +0200

    [Anagramos] - Ajout du formulaire pour l'envoie des données - Ajout de la récuperation des informations du formulaire
    [Static Data]
    - Déplacement des lists de mots dans le dossier "static"

--- application/apps/anagramos/routes.py
+++ application/apps/anagramos/routes.py
@@ -1,24 +1,32 @@
 from flask import render_template, make_response, request
 
-from application.apps.anagramos import anagramos_bp
+from application.apps.anagramos import anagramos_bp, anagramos_source
+from application.apps.common_data import apps_toolbox
 
 import concurrent.futures
 
 
 ############################################
-#                A                      #
+#                Anagramos                 #
 ############################################
 
 @anagramos_bp.route('/anagramos', methods=['GET', 'POST'])
 def anagramos():
     if request.method == "POST":
-        # Todo -> Rajouter la reception du formulaire
-        word = ""
+        word = request.form['word']
+
+        lang = request.form['language-select']
         words = []
+        if lang == "French":  # language_enum.Language.FRENCH:
+            words = apps_toolbox.get_french_words()
+        elif lang == "English":  # language_enum.Language.ENGLISH:
+            words = apps_toolbox.get_english_words()
+        if not words:
+            return make_response(render_template('anagramos.html', title="Anagramos", error="No language selected"), 200)
 
         with concurrent.futures.ThreadPoolExecutor() as executor:
-            future = executor.submit(anagramos.find_anagrammes, word, words)
+            future = executor.submit(anagramos_source.find_anagrammes, word, words)
             output_word_list = future.result()
-            return make_response(render_template('anagramos.html', title="Anagramos", words_list=output_word_list), 200)
+            return make_response(render_template('anagramos.html', title="Anagramos", results=output_word_list, word=word), 200)
     else:
         return make_response(render_template('anagramos.html', title="Anagramos"), 200)
--- application/apps/common_data/apps_toolbox.py
+++ application/apps/common_data/apps_toolbox.py
@@ -1,4 +1,7 @@
+import os
 from typing import Set
+
+from flask import current_app
 
 
 def compare_letter_count_one_liner(word_1: str, word_2: str) -> bool:
@@ -28,12 +31,39 @@ def compare_letter_count(word_1: str, word_2: str) -> bool:
     return all(output)
 
 
-def get_words_from_file(fname: str) -> Set[str]:
+def get_words_from_file(fname: str, charset="utf-8") -> Set[str]:
     """
     Parse a file and return a set of the words in it
+    :param charset: Charset of the given file
     :param fname: File to parse
     :return: A set of words from the parsed file
     """
-    with open(fname) as word_file:
-        words = set(word_file.read().split())
+    with current_app.open_resource(fname) as word_file:
+        data = word_file.read().decode(charset)
+        words = set(data.split())
     return words
+
+
+# Folder where the list of words are stored
+folder = "static/words_lists"
+
+
+def get_english_words() -> Set[str]:
+    fname = os.path.join(folder, "english_words.txt")
+    """
+    Returns the
+    :param fname: File name
+    :return: 
+    """
+    return get_words_from_file(fname)
+
+
+def get_french_words() -> Set[str]:
+    # fname = os.path.join(APP_STATIC, folder, "french_words.txt")
+    fname = os.path.join(folder, "french_words.txt")
+    """
+    
+    :param fname: 
+    :return: 
+    """
+    return get_words_from_file(fname, charset="iso-8859-1")
