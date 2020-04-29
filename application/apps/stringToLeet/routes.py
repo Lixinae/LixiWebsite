4207841711f320c3088e67661d392c495dbaf3c9
Vahen <naeos@laposte.net>
2020-04-29 17:17:33 +0200

    [Apps]     [StringToLeet]     - Ajout du front-end de l'application     [Anagramos]     - Correction de l'affichage

--- application/apps/stringToLeet/routes.py
+++ application/apps/stringToLeet/routes.py
@@ -2,23 +2,27 @@ import concurrent.futures
 
 from flask import render_template, make_response, request
 
-from application.apps.stringToLeet import string_to_leet_bp
+from application.apps.stringToLeet import string_to_leet_bp, stringToLeet_source
 
 
 ############################################
-#                Apps                      #
+#             String to leet               #
 ############################################
 
 @string_to_leet_bp.route('/string_to_leet', methods=['GET', 'POST'])
 def string_to_leet():
     # Todo -> method == POST / GET
     if request.method == "POST":
-        # Todo -> Rajouter la reception du formulaire
-        phrase_to_translate = ""
-
+        phrase = request.form['phrase']
+        if not phrase:
+            return make_response(render_template('anagramos.html', title="Anagramos", error="No word in input"), 200)
         with concurrent.futures.ThreadPoolExecutor() as executor:
-            future = executor.submit(string_to_leet, phrase_to_translate)
-            translated_string = future.result()
-            return make_response(render_template('stringToLeet.html', title="string_to_leet", translated_string=translated_string), 200)
+            future = executor.submit(stringToLeet_source.string_to_leet, phrase)
+            phrase_translated = future.result()
+            return make_response(render_template('stringToLeet.html',
+                                                 title="string_to_leet",
+                                                 phrase=phrase,
+                                                 phrase_translated=phrase_translated),
+                                 200)
     else:
         return make_response(render_template('stringToLeet.html', title="string_to_leet"), 200)
