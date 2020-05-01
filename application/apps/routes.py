dede02325c7d36e73a3b5c24fab1dfa2ad82634c
Vahen <naeos@laposte.net>
2020-05-01 18:08:58 +0200

    [Apps] - Correction routes - Correction erreur sur bouton apps
    [CSS]
    - Ajout de transition
    - Corrections de couleurs

--- application/apps/routes.py
+++ application/apps/routes.py
@@ -2,9 +2,6 @@ from flask import render_template, make_response
 
 from application.apps import apps_bp
 
-from application.passions import data as data_passions
-from application.portfolio import data as data_portfolio
-
 from application.apps.data import app_list
 
 
@@ -14,5 +11,4 @@ from application.apps.data import app_list
 
 @apps_bp.route('/apps')
 def apps():
-    apps_list = app_list()
-    return make_response(render_template('apps.html', title="Home", apps_list=apps_list), 200)
+    return make_response(render_template('apps.html', title="Apps", app_list=app_list()), 200)
