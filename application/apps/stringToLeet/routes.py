f34fbc4bc1053dad84c5fe13f26bb1961c7390a5
Vahen <naeos@laposte.net>
2020-06-11 19:14:26 +0200

    [String To Leet] - Passage de la requete d'envoie en AJAX - Adaptation du code python avec flask_restx - Creation du fichier JS associé à l'application

--- application/apps/stringToLeet/routes.py
+++ application/apps/stringToLeet/routes.py
@@ -1,28 +1,26 @@
 import concurrent.futures
 
-from flask import render_template, make_response, request
+from flask import render_template, make_response, request, jsonify
 
-from application.apps.stringToLeet import string_to_leet_bp, stringToLeet_source
+from application.apps.stringToLeet import string_to_leet_api, stringToLeet_source
+from flask_restx import Resource
 
 
 ############################################
 #             String to leet               #
 ############################################
 
-@string_to_leet_bp.route('/', methods=['GET', 'POST'])
-def string_to_leet():
-    if request.method == "POST":
+@string_to_leet_api.route('/')
+class StringToLeetPage(Resource):
+    def get(self):
+        return make_response(render_template('stringToLeet.html', title="string_to_leet"), 200)
+
+    def post(self):
         phrase = request.form['phrase']
         if not phrase:
-            return make_response(render_template('anagramos.html', title="Anagramos", error="No word in input"), 200)
+            return make_response(render_template('stringToLeet.html', title="string_to_leet", error="No word in input"), 200)
         with concurrent.futures.ThreadPoolExecutor() as executor:
             future = executor.submit(stringToLeet_source.string_to_leet, phrase)
             phrase_translated = future.result()
             # Todo -> Utiliser l'ajax coté client et faire la réponse
-            return make_response(render_template('stringToLeet.html',
-                                                 title="string_to_leet",
-                                                 phrase=phrase,
-                                                 phrase_translated=phrase_translated),
-                                 200)
-    else:
-        return make_response(render_template('stringToLeet.html', title="string_to_leet"), 200)
+            return jsonify({"results": phrase_translated})
