6500d7faf27aa312d283d35c6a50b7408dde6f75
Vahen <naeos@laposte.net>
2020-06-11 19:17:07 +0200

    [Logger] - Amélioration des logs de démarrage

--- application/logger.py
+++ application/logger.py
@@ -12,12 +12,15 @@ def setup_logging():
     path = os.path.join(dir_parent_path, "loggingConfig.yaml")
     # Creation du dossier pour les logs dans le cas ou il n'existe pas
     logs_folder = os.path.join(dir_parent_path, "logs")
-    try:
-        os.mkdir(logs_folder)
-    except OSError:
-        print("Creation of the directory %s failed" % logs_folder)
+    if os.path.exists(logs_folder):
+        print("Folder : %s already exists" % logs_folder)
     else:
-        print("Successfully created the directory %s " % logs_folder)
+        try:
+            os.mkdir(logs_folder)
+        except OSError:
+            print("Creation of the directory %s failed" % logs_folder)
+        else:
+            print("Successfully created the directory %s " % logs_folder)
     with open(path, 'rt') as file:
         config = yaml.safe_load(file.read())
         logging.config.dictConfig(config)
