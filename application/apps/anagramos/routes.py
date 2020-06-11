8dbd987915df181f7a3e89bc3dfc9eac48558dad
Vahen <naeos@laposte.net>
2020-06-11 19:12:17 +0200

    [Anagramos] - Corrections du javascript - Ajout d'un bouton pour cacher les résultats - Suppression de code inutile

--- application/apps/anagramos/routes.py
+++ application/apps/anagramos/routes.py
@@ -13,29 +13,28 @@ import concurrent.futures
 @anagramos_api.route('/')
 class AnagramosPage(Resource):
     def get(self):
-        if request.method == "GET":
-            return make_response(render_template('anagramos.html', title="Anagramos"), 200)
+        return make_response(render_template('anagramos.html', title="Anagramos"), 200)
 
     def post(self):
-        if request.method == "POST":
-            """
-            Dans la requete "POST" on récupère les du formulaire
-            Champs : 'word', 'language-select'
-            """
-            word = request.form['word']
-            lang = request.form['language-select']
-            words = []
-            if lang == "French":
-                words = apps_toolbox.get_french_words()
-            elif lang == "English":
-                words = apps_toolbox.get_english_words()
-            if not words:
-                return make_response(render_template('anagramos.html', title="Anagramos", error="No language selected"), 200)
-            with concurrent.futures.ThreadPoolExecutor() as executor:
-                future = executor.submit(anagramos_source.find_anagrammes, word, words)
-                output_word_list = future.result()
-                # On ne veut pas donner le mot dont on cherche les anagrames
-                if word in output_word_list:
-                    output_word_list.remove(word)
-            if output_word_list:
-                return jsonify({"results": output_word_list})
+        """
+        Dans la requete "POST" on récupère les champs du formulaire
+        Champs : 'word', 'language-select'
+        """
+        word = request.form['word']
+        lang = request.form['language-select']
+        words = []
+        if lang == "French":
+            words = apps_toolbox.get_french_words()
+        elif lang == "English":
+            words = apps_toolbox.get_english_words()
+        if not words:
+            return make_response(render_template('anagramos.html', title="Anagramos", error="No language selected"), 200)
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            future = executor.submit(anagramos_source.find_anagrammes, word, words)
+            output_word_list = future.result()
+            # On ne veut pas donner le mot dont on cherche les anagrames
+            if word in output_word_list:
+                output_word_list.remove(word)
+        if output_word_list:
+            return jsonify({"results": output_word_list})
+        return jsonify({"results": []})
