5dddc1abfecba0af20e98ba6faa9f6459f6dbde8
Vahen <naeos@laposte.net>
2020-08-02 16:54:06 +0200

    [StringToLeet] - Utilisation de vue JS plutôt que Jquery - Ajout de la sauvegarde des champs du formulaire sur la session

--- application/apps/stringToLeet/routes.py
+++ application/apps/stringToLeet/routes.py
@@ -16,11 +16,11 @@ class StringToLeetPage(Resource):
         return make_response(render_template('stringToLeet.html', title="string_to_leet"), 200)
 
     def post(self):
-        phrase = request.form['phrase']
+        post_data = request.get_json()
+        phrase = post_data['phrase']
         if not phrase:
             return make_response(render_template('stringToLeet.html', title="string_to_leet", error="No word in input"), 200)
         with concurrent.futures.ThreadPoolExecutor() as executor:
             future = executor.submit(stringToLeet_source.string_to_leet, phrase)
             phrase_translated = future.result()
-            # Todo -> Utiliser l'ajax coté client et faire la réponse
             return jsonify({"results": phrase_translated})
--- application/apps/stringToLeet/static/scripts/string_to_leet.js
+++ application/apps/stringToLeet/static/scripts/string_to_leet.js
@@ -1,37 +1,59 @@
-/**
- * Methode post sur le formulaire du string to leet
- **/
-$("#string_to_leet_post_form").submit(function (event) {
-    event.preventDefault(); //prevent default action
-    let post_url = $(this).attr("action"); //get form action url
-    let form_data = $(this).serialize(); //Encode form elements for submission
-    let stringToLeetResult = $(".stringToLeet-results");
-    let stringToLeetWrapper = $(".stringToLeet-results-wrapper");
-    /* Lancement de la methode post */
-    $.post(post_url, form_data)
-        /* Si le serveur renvoie une réponse */
-        .done(function (response) {
-            if (stringToLeetWrapper.hasClass("hidden")) {
-                stringToLeetWrapper.toggleClass("hidden");
-            }
-            let word = $("#word-box").val();
-            stringToLeetResult.html("");
-            stringToLeetResult.prepend("<h3> String to leet result </h3>");
-            let results_html = '<div class="stringToLeet-result-wrapper">' +
-                '<p> Original : ' + word + ' </p>' +
-                '<p> Traduction :' + response.results + ' </p>' +
-                '</div>';
-            stringToLeetResult.append(results_html);
-        })
-        /* En cas d'erreur */
-        .fail(function () {
-            if (stringToLeetWrapper.hasClass("hidden")) {
-                stringToLeetWrapper.toggleClass("hidden");
-            }
-            stringToLeetResult.text("Error: Could not contact server");
-        });
+// N'utiliser qu'une seul vue par fichier, éviter les Vue multiple
+const vueStringToLeet = new Vue({
+    el: '#string_to_leet_post_form_wrapper',
+    data: {
+        phrase: '',
+        response_data: [],
+        data_history: [],
+        show_results: false,
+        request_done: false
+    },
+    methods: {
+        postData: function () {
+            console.log({phrase: this.phrase});
+            let post_url = $("#string_to_leet_post_form").attr("action"); //get form action url
+            let self = this; // Permet d'utiliser le this de la Vue
+            axios.post(post_url, {phrase: this.phrase})
+                .then(function (response) {
+                    // Attention au this ici -> Le this ici est celui de Axios et pas de l'objet Vue
+                    self.response_data = response.data.results;
+                    self.show_results = true;
+                    self.request_done = true;
+                    self.data_history.push({
+                        original: self.phrase,
+                        translation: response.data.results
+                    });
+                    console.log(self.data_history);
+                })
+                .catch(error => {
+                    console.log(error)
+                });
+            console.log(this.data_history)
+        }
+    },
+    delimiters: ["<%", "%>"],
+    mounted() {
+        if (localStorage.phrase) {
+            this.phrase = localStorage.phrase;
+        }
+        if (localStorage.data_history) {
+            console.log("Mounted");
+            console.log(localStorage.data_history);
+            //localStorage.data_history = []
+            localStorage.clear();
+            this.data_history = Object.keys(localStorage.data_history).filter(key => localStorage.data_history[key]);
+        }
+    },
+    watch: {
+        phrase(newPhrase) {
+            localStorage.phrase = newPhrase;
+        },
+        data_history(newDataHistory) {
+            //console.log("Hello");
+            //console.log(newDataHistory);
+            //localStorage.clear();
+            //localStorage.data_history = Object.keys(newDataHistory).filter(key => newDataHistory[key]);
+            //localStorage.data_history = []
+        }
+    }
 });
-
-function displayResults() {
-    $(".stringToLeet-results-wrapper").toggleClass("hidden");
-}
