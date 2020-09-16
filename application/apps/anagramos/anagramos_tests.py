6561092695461004031886c6249420225842dda0
Vahen <naeos@laposte.net>
2020-09-16 22:00:27 +0200

    [Apps]   [Anagramos]     - Ajout du squelette pour les tests api / unitaire

---
+++ application/apps/anagramos/anagramos_tests.py
@@ -0,0 +1,21 @@
+import unittest
+
+
+class TestAnagramosSource(unittest.TestCase):
+    def test_find_anagrammes(self):
+        pass
+
+
+class TestAnagramosAPI(unittest.TestCase):
+    def test_get_data(self):
+        pass
+
+    def test_post_data_ok(self):
+        pass
+
+    def test_post_data_wrong(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
