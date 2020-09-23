15a7fc8fb9343f92fb08fa0273435833b1eabfa1
Vahen <Vahen@users.noreply.github.com>
2020-09-23 14:09:08 +0200

    Update anagramos_tests.py

--- application/apps/anagramos/anagramos_tests.py
+++ application/apps/anagramos/anagramos_tests.py
@@ -3,17 +3,21 @@ import unittest
 
 class TestAnagramosSource(unittest.TestCase):
     def test_find_anagrammes(self):
+        # Todo
         pass
 
 
 class TestAnagramosAPI(unittest.TestCase):
     def test_get_data(self):
+        # Todo
         pass
 
     def test_post_data_ok(self):
+        # Todo
         pass
 
     def test_post_data_wrong(self):
+        # Todo
         pass
 
 
