a63786ddd32602027b82824de43a7e80831e6a12
Vahen <Vahen@users.noreply.github.com>
2020-09-23 14:08:41 +0200

    Update stringToLeet_tests.py

--- application/apps/stringToLeet/stringToLeet_tests.py
+++ application/apps/stringToLeet/stringToLeet_tests.py
@@ -30,12 +30,15 @@ class TestStringToLeetSource(unittest.TestCase):
 
 class TestStringToLeetAPI(unittest.TestCase):
     def test_get_data(self):
+        # Todo
         pass
 
     def test_post_data_ok(self):
+        # Todo
         pass
 
     def test_post_data_wrong(self):
+        # Todo
         pass
 
 
