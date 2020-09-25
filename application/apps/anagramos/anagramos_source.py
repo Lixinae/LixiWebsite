64a7c516e95a526ed29e81c9cee6d90d8e0b09f8
Vahen <naeos@laposte.net>
2020-09-25 20:03:42 +0200

    [Apps]   [Anagramos]   - Corrections du type de find anagrammes   - Deplacement de fonction a un endroit plus logique   - Ajout de test unitaire   - Corrections de test api

--- application/apps/anagramos/anagramos_source.py
+++ application/apps/anagramos/anagramos_source.py
@@ -1,13 +1,27 @@
-from typing import List
-
-from application.apps.common_data import apps_toolbox
+from typing import List, Set
 
 
-def find_anagrammes(word: str, words: str) -> List[str]:
+def compare_letter_count(origin: str, compared_to: str) -> bool:
+    """
+    Compares word_1 and word_2, returns True if they are anagrames
+    :param origin: First word
+    :param compared_to: Second word
+    :return: True if origin and compared_to have the same length and the same amount of each letters
+    """
+    if type(origin) != str:
+        return False
+    origin = origin.lower().replace(" ", "")
+    compared_to = compared_to.lower().replace(" ", "")
+    if len(origin) != len(compared_to):
+        return False
+    return all([origin.count(letter) == compared_to.count(letter) for letter in origin])
+
+
+def find_anagrammes(word: str, words: Set[str]) -> List[str]:
     """
     Take a word and finds all the anagrames of it
     :param word: Word for which we want to find the anagrames
     :param words: List of words where to find the anagrames
     :return: A list of all the anagrames of the given word
     """
-    return [w for w in words if apps_toolbox.compare_letter_count(w, word)]
+    return [w for w in words if compare_letter_count(w, word)]
--- application/apps/anagramos/anagramos_tests.py
+++ application/apps/anagramos/anagramos_tests.py
@@ -1,11 +1,45 @@
 import unittest
 
 from application import create_app, TestingConfig
+from application.apps.anagramos.anagramos_source import find_anagrammes
 
 
 class TestAnagramosUnitTest(unittest.TestCase):
-    def test_find_anagrammes(self):
-        # Todo
+    def test_find_anagrammes_ok(self):
+        word = "étoile"
+        words = {"étoile", "étiole", "soufflet", "bidule", "machin", "chose"}
+        anagrams = find_anagrammes(word, words)
+        self.assertEqual(list, type(anagrams))
+        self.assertEqual(2, len(anagrams))
+
+    def test_find_anagrammes_not_ok(self):
+        word = "étoile"
+        words = {"soufflet", "bidule", "machin", "chose"}
+        anagrams = find_anagrammes(word, words)
+        self.assertEqual(list, type(anagrams))
+        self.assertEqual(0, len(anagrams))
+
+    def test_find_anagrammes_numbers_in_list(self):
+        word = "étoile"
+        words = {1, "bidule", 3, "chose"}
+        anagrams = find_anagrammes(word, words)
+        self.assertEqual(list, type(anagrams))
+        self.assertEqual(0, len(anagrams))
+
+    def test_find_anagrammes_word_and_set_empty(self):
+        word = ""
+        words = {}
+        anagrams = find_anagrammes(word, words)
+        self.assertEqual(list, type(anagrams))
+        self.assertEqual(0, len(anagrams))
+
+    def test_compare_letter_count_ok(self):
+        pass
+
+    def test_compare_letter_count_different_size(self):
+        pass
+
+    def test_compare_letter_count_wrong_type(self):
         pass
 
 
@@ -18,7 +52,8 @@ class TestAnagramosAPI(unittest.TestCase):
     def test_post_data_ok(self):
         test_app = create_app(TestingConfig).test_client()
         response = test_app.post("/apps/anagramos/", json={
-
+            "word": "étoile",
+            "language_select": "French"
         })
         self.assertEqual(200, response.status_code)
         self.assertEqual(dict, type(response.json))
