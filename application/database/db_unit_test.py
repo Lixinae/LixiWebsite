66a041877bbbd0cec2dc32494c9198867d2df352
Vahen <naeos@laposte.net>
2021-01-15 00:32:10 +0100

    [Database] - Correction du set up des TU

--- application/database/db_unit_test.py
+++ application/database/db_unit_test.py
@@ -1,11 +1,12 @@
 import unittest
 
-from application import TestingConfig, create_app,db
+from application import TestingConfig, create_app, db
+from application.apps.data import app_list
 
 
 class DbUnitTest(unittest.TestCase):
     def setUp(self):
-
+        self.test_app = create_app(TestingConfig).test_client()
         db.create_all()
 
     def tearDown(self):
@@ -13,6 +14,5 @@ class DbUnitTest(unittest.TestCase):
         db.drop_all()
 
     def test_something(self):
-        test_app = create_app(TestingConfig).test_client()
-
+        pass
         self.assertEqual(True, False)
