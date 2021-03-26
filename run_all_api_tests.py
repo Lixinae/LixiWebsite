9503e3d33f5329f8999ae6943f15af055db4b900
Vahen <chris.xelo.pico@gmail.com>
2021-03-26 16:02:37 +0100

    [Test API] - Ajout des test api manquant au scripts qui les lance tous

--- run_all_api_tests.py
+++ run_all_api_tests.py
@@ -4,14 +4,23 @@ import unittest
 from application.apps.anagramos.anagramos_tests import TestAnagramosAPI
 from application.apps.string_to_leet.string_to_leet_tests import TestStringToLeetAPI
 from application.apps.pendu.pendu_tests import TestPenduAPI
+from application.passions.travail_du_cuir_api.travail_du_cuir_tests import TestTravailDuCuirAPI
+from application.portfolio.portfolio_tests import PortfolioTestApi
+
 
 def create_suite_api():
     suite_anagramos = unittest.TestLoader().loadTestsFromTestCase(TestAnagramosAPI)
     suite_string_to_leet = unittest.TestLoader().loadTestsFromTestCase(TestStringToLeetAPI)
     suite_pendu = unittest.TestLoader().loadTestsFromTestCase(TestPenduAPI)
+    suite_portfolio = unittest.TestLoader().loadTestsFromTestCase(PortfolioTestApi)
+    suite_tdc = unittest.TestLoader().loadTestsFromTestCase(TestTravailDuCuirAPI)
 
     # Todo Ajouter les autres test API une fois qu'ils seront codé
-    alltests = unittest.TestSuite([suite_anagramos, suite_string_to_leet, suite_pendu])
+    alltests = unittest.TestSuite([suite_anagramos,
+                                   suite_string_to_leet,
+                                   suite_pendu,
+                                   suite_portfolio,
+                                   suite_tdc])
     return alltests
 
 
@@ -22,4 +31,3 @@ if __name__ == '__main__':
     result = runner.run(suite)
     if not result.wasSuccessful():
         sys.exit(1)
-
